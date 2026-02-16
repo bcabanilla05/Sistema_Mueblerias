99cb1a6feee7279f5fae96e554ca56541c168042
bruno <bruno@BRUNO>
2026-02-15 23:54:48 -0500

    Primer commit - Sistema Mueblerias

---
+++ src/main/java/com/mycompany/sistema_mueblerias/Sistema_Mueblerias.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistema_mueblerias;
+
+/**
+ *
+ * @author bruno
+ */
+public class Sistema_Mueblerias {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
